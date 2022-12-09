a41d1c36128c855287ef82d5a84b47c18150d354
= <=>
2022-12-09 15:51:41 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int count=1,odd=0,even=0 ;
+     while (count !=0) {
+         cout << "Enter an integer: ";
+         cin>> count ;
+    if(count!=0){
+        if(count%2!=0){
+        odd++ ;
+    }   else {
+            even++;
+            }   
+     }
+     }
+    cout << "#Even numbers = "<<even<<"\n";
+    cout << "#Odd numbers = "<<odd ;
+
+
     return 0;
+
 }
